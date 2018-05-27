2719e5a83f496a5bf045d7f72c46b5c7307911f6
Sherry Goyal <shgoyal@DELC02SN8XRG8WN.sea.corp.expecn.com>
2018-05-27 14:02:26 +0530

    Number formattor added as per Internal number system

--- product/src/main/java/com/expedia/risk/report/generator/confluence/ConfluenceCreator.java
+++ product/src/main/java/com/expedia/risk/report/generator/confluence/ConfluenceCreator.java
@@ -10,14 +10,13 @@ import org.springframework.web.client.RestTemplate;
 
 import com.expedia.risk.report.generator.model.Body;
 import com.expedia.risk.report.generator.model.ConfluenceRequest;
-import com.expedia.risk.report.generator.util.SSLUtil;
-import com.expedia.risk.report.generator.model.Space;
-import com.expedia.risk.report.generator.model.Storage;
-import com.expedia.risk.report.generator.model.Version;
 import com.expedia.risk.report.generator.model.Field;
 import com.expedia.risk.report.generator.model.Report;
 import com.expedia.risk.report.generator.model.Service;
+import com.expedia.risk.report.generator.model.Space;
+import com.expedia.risk.report.generator.model.Storage;
 import com.expedia.risk.report.generator.model.WeeklyDetails;
+import com.expedia.risk.report.generator.util.SSLUtil;
 
 public class ConfluenceCreator
 {
@@ -66,7 +65,7 @@ public class ConfluenceCreator
             final String SERVER_URI = "https://confluence/rest/api/content";
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
-            headers.add("Authorization", "Basic Z2F1amFpbjpwcmFzbmF0aDEhMTIzNDU2Nw==");
+            headers.add("Authorization", "Basic ");
             headers.add("contentType", "application/json");
             Report report = populateRequestWith2Weeks();
             ConfluenceRequest request = renderConfluencePage(report);
@@ -234,7 +233,7 @@ public class ConfluenceCreator
         {
             Report report = new Report();
             report.setEmailId("abc@expedia.com");
-            report.setReportName("Fraud Ops Report 2018-05-21 - 2018-05-27_Mapper_10");
+            report.setReportName("Fraud Ops Report 2018-05-21 - 2018-05-27_Mapper_11");
             report.setNoOfWeeks(2);
             ArrayList<Service> servicesList = new ArrayList<>(1);
             List<WeeklyDetails> weeklyDetailsList = new ArrayList<>(2);
@@ -244,16 +243,15 @@ public class ConfluenceCreator
             service.setServiceName("ListService");
 
             WeeklyDetails weeklyDetails = new WeeklyDetails();
-            weeklyDetails.setQuery(
-                    "index=fraud sourcetype=risk_listservice_perf ActivityName=endpoint* ActivityStep=rs (OperationName=lookup OR OperationName=addItems OR OperationName=items) | stats count, exactperc99(Duration), exactperc99.9(Duration) by OperationName, RequestMethod");
+            weeklyDetails.setQuery("index=fraud sourcetype=risk_listservice_perf ActivityName=endpoint* ActivityStep=rs (OperationName=lookup OR OperationName=addItems OR OperationName=items) | stats count, exactperc99(Duration), exactperc99.9(Duration) by OperationName, RequestMethod");
             weeklyDetails.setExtraColumns("SLA in ms", new String[]{"250", "5,000", "1,000", "1,000"});
             weeklyDetails.setExtraColumns("Comments", null);
             weeklyDetails.setTableName("Operational Metrics");
             ArrayList<String> resultList = new ArrayList<>(4);
-            resultList.add("lookup:62,613,025#62,047,872:5#4:179#171:250: ");
-            resultList.add("items (fetch all items in a list):305#411:759#1,465:1,258#2,455:5000: ");
-            resultList.add("items (single item add):370#384:59#42:74#119:1,000: ");
-            resultList.add("addItems (multiple items add):12,885#14,966:264#330:472#520:1,000: ");
+            resultList.add("lookup:62613025#62047872:5#4:179#171:250: ");
+            resultList.add("items (fetch all items in a list):305#411:759#1465:1258#2455:5000: ");
+            resultList.add("items (single item add):370#384:59#42:74#119:1000: ");
+            resultList.add("addItems (multiple items add):12885#14966:264#330:472#520:1000: ");
             weeklyDetails.setResults(resultList);
             ArrayList<Field> fieldList = new ArrayList<>();
             Field field = new Field();
@@ -283,9 +281,9 @@ public class ConfluenceCreator
             weeklyDetails.setTableName("Lookup Requests per Client ");
             resultList = new ArrayList<>(4);
             resultList.add("FraudWS:7#8: ");
-            resultList.add("VCS:51,130,424#50,893,516: ");
-            resultList.add("VOYAGER:10,937,761#10,734,479: ");
-            resultList.add("Zeus:544,833#541,502: ");
+            resultList.add("VCS:51130424#50893516: ");
+            resultList.add("VOYAGER:10937761#10734479: ");
+            resultList.add("Zeus:544833#541502: ");
             weeklyDetails.setResults(resultList);
             fieldList = new ArrayList<>();
             field = new Field();
@@ -311,13 +309,13 @@ public class ConfluenceCreator
                     "index=fraud sourcetype=\"fraudws_perf\"  ActivityName=\"endpoint*\" ActivityStep=rs "
                             + "(OperationName=sladetails OR OperationName=cancelOrder OR OperationName=release) | "
                             + "stats count, exactperc99(Duration), exactperc99.9(Duration) by OperationName");
-            weeklyDetails.setExtraColumns("SLA", new String[]{"400","90,000","90,000"});
+            weeklyDetails.setExtraColumns("SLA", new String[]{"400","90000","90000"});
             weeklyDetails.setExtraColumns("Comments", null);
             weeklyDetails.setTableName("Operational Metrics ");
             resultList = new ArrayList<>(4);
-            resultList.add("sladetails:6,298,551#6,433,023:14#19:42#60:400: ");
-            resultList.add("cancelOrder:6,298,551#6,433,023:14#19:42#60:90,000: ");
-            resultList.add("release:6,298,551#6,433,023:14#19:42#60:90,000: ");
+            resultList.add("sladetails:6298551#6433023:14#19:42#60:400: ");
+            resultList.add("cancelOrder:6298551#6433023:14#19:42#60:90000: ");
+            resultList.add("release:6298551#6433023:14#19:42#60:90000: ");
             weeklyDetails.setResults(resultList);
             fieldList = new ArrayList<>();
             field = new Field();
